762ad322eb57110b33e9e78a08873ecec7d820b2
Tran Thien An PHAN <phantranthienan1405@gmail.com>
2024-10-11 18:44:55 +0200

    add base frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,38 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-export default App
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+
+import { AppLayout, AuthLayout } from './layout';
+import { Home, Login, Register, Board } from './pages';
+import { darkTheme } from './themes';
+
+
+
+const App = () => {
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline enableColorScheme />
+			<Router>
+				<Routes>
+					<Route path='/' element={<AuthLayout />}>
+						<Route path='login' element={<Login />} />
+						<Route path='register' element={<Register />} />
+					</Route>
+					<Route path='/' element={<AppLayout />}>
+						<Route index element={<Home />} />
+						<Route path='boards' element={<Home />} />
+						<Route path='boards/:boardId' element={<Board />} />
+					</Route>
+				</Routes>
+			</Router>
+		</ThemeProvider>
+	);
+};
+
+export default App;
---
+++ frontend/src/themes/themes.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const ligthTheme = createTheme({
+    palette: {
+        mode: 'light',
+    },
+});
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+})
